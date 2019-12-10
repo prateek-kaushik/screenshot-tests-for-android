14446ab5e5b5d4befa51b81fbafadf467dc8bd9c
Peerapathananont, Kajornsak (Agoda) <kajornsak.peerapathananont@agoda.com>
2019-12-10 23:40:12 +0700

    add structured path

--- plugin/src/py/android_screenshot_tests/recorder.py
+++ plugin/src/py/android_screenshot_tests/recorder.py
@@ -43,34 +43,38 @@ class Recorder:
         with Image.open(file_name) as im:
             return im.size
 
-    def _copy(self, name, w, h):
+    def _copy(self, name, classname, method, w, h):
+        img_path = join(self._input, classname)
         tilewidth, tileheight = self._get_image_size(
-            join(self._input,
+            join(img_path,
                  common.get_image_file_name(name, 0, 0)))
 
         canvaswidth = 0
 
         for i  in range(w):
             input_file = common.get_image_file_name(name, i, 0)
-            canvaswidth += self._get_image_size(join(self._input, input_file))[0]
+            canvaswidth += self._get_image_size(join(img_path, input_file))[0]
 
 
         canvasheight = 0
 
         for j in range(h):
             input_file = common.get_image_file_name(name, 0, j)
-            canvasheight += self._get_image_size(join(self._input, input_file))[1]
+            canvasheight += self._get_image_size(join(img_path, input_file))[1]
 
         im = Image.new("RGBA", (canvaswidth, canvasheight))
 
         for i in range(w):
             for j in range(h):
                 input_file = common.get_image_file_name(name, i, j)
-                with Image.open(join(self._input, input_file)) as input_image:
+                with Image.open(join(img_path, input_file)) as input_image:
                     im.paste(input_image, (i * tilewidth, j * tileheight))
                     input_image.close()
 
-        im.save(join(self._output, name + ".png"))
+        output_path = join(self._output, classname)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        im.save(join(output_path, method + ".png"))
         im.close()
 
     def _get_metadata_root(self):
@@ -79,7 +83,11 @@ class Recorder:
     def _record(self):
         root = self._get_metadata_root()
         for screenshot in root.iter("screenshot"):
+            test_class = screenshot.find('test_class').text
+            test_method = screenshot.find('test_name').text
             self._copy(screenshot.find('name').text,
+                       test_class,
+                       test_method,
                        int(screenshot.find('tile_width').text),
                        int(screenshot.find('tile_height').text))
 
@@ -88,7 +96,7 @@ class Recorder:
             shutil.rmtree(self._output)
         os.makedirs(self._output)
 
-    def _is_image_same(self, file1, file2, failure_file):
+    def _is_image_same(self, name, file1, file2, failure_folder, failure_file):
         with Image.open(file1) as im1, Image.open(file2) as im2:
             diff_image = ImageChops.difference(im1, im2)
             try:
@@ -97,10 +105,12 @@ class Recorder:
                     return True
                 else:
                     if failure_file:
+                        if not os.path.exists(failure_folder):
+                            os.makedirs(failure_folder)
                         diff_list = list(diff) if diff else []
                         draw = ImageDraw.Draw(im2)
                         draw.rectangle(diff_list, outline = (255,0,0))
-                        im2.save(failure_file)
+                        im2.save(join(failure_folder, failure_file))
                     return False
             finally:
                 diff_image.close()
@@ -117,18 +127,20 @@ class Recorder:
         failures = []
         for screenshot in root.iter("screenshot"):
             name = screenshot.find('name').text + ".png"
-            actual = join(self._output, name)
-            expected = join(self._realoutput, name)
+            test_class = screenshot.find('test_class').text
+            test_method = screenshot.find('test_name').text
+            actual = join(join(self._output, test_class), test_method + ".png")
+            expected = join(join(self._realoutput, test_class), test_method + ".png")
             if self._failure_output:
                 diff_name = screenshot.find('name').text + "_diff.png"
-                diff = join(self._failure_output, diff_name)
-                
-                if not self._is_image_same(expected, actual, diff):
+                diff = join(self._failure_output, test_class)
+                is_passed = self._is_image_same(test_method, expected, actual, diff, diff_name)
+                if not is_passed:
                     expected_name = screenshot.find('name').text + "_expected.png"
                     actual_name = screenshot.find('name').text + "_actual.png"
 
-                    shutil.copy(actual, join(self._failure_output, actual_name))
-                    shutil.copy(expected, join(self._failure_output, expected_name))
+                    shutil.copy(actual, join(diff, actual_name))
+                    shutil.copy(expected, join(diff, expected_name))
                     
                     failures.append((expected, actual))
             else:
